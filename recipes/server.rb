5972f7426c7ead75e418c7976b00aa5b5aa18399
ravivittal <rvittal_dba@yahoo.com>
2017-09-13 06:45:16 +0000

    Release Version 0.2.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,14 +4,20 @@
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
+include_recipe 'apache::server'
+
 package 'httpd' do
   action :install
 end
 
 file '/var/www/html/index.html' do 
-  content '<h1>Hello World</h1>'
+  content "<h1>Hello World</h1>
+  <h2>ipaddress: #{node['ipaddress']}</h2>
+  <h2>hostname: #{node['hostname']}</h2>"
 end
 
 service 'httpd' do
   action [ :enable, :start ]
 end
+
+#include_recipe 'apache::server'
